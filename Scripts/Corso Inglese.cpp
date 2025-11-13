7a7e19e2f0aedaa1bff5bfeba031c7c733b27f8c
TheDiegoni <thediegoni575@gmail.com>
2025-11-13 15:57:36 +0100

    Update Corso Inglese.cpp

--- Scripts/Corso Inglese.cpp
+++ Scripts/Corso Inglese.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int NUM1, NUM2;
+float X, Y, RIC;
+const float REFM=0.9;
+
+void main(){
+  cout << "Studenti Livello 1: ";
+  cin >> NUM1;
+  cout << "Costo Livello 1: ";
+  cin >> X;
+  X *= NUM1;
+  cout << "Studenti Livello 2: ";
+  cin >> NUM2;
+  cout << "Costo Livello 2: ";
+  cin >> Y;
+  Y *= NUM2;
+
+  RIC = (X+Y)*REFM;
+  cout << "Guadagno: " << RIC;
+}
