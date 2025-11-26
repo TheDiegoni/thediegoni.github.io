51359073910026a60317d9f8489b8a1c9e651b85
TheDiegoni <thediegoni575@gmail.com>
2025-11-26 09:46:25 +0100

    Refactor ticket pricing logic in Stadio.cpp
    Fix formatting and structure of age-based ticket pricing logic.

--- Scripts/Stadio.cpp
+++ Scripts/Stadio.cpp
@@ -1,22 +1,34 @@
+/*
+	Autore: Diego Rocco
+	Descrizione: Allo stadio il costo del biglietto è gratis fino a 10 anni e sopra i 65,
+	costa 5 euro fino a 18 anni e 10 euro per tutti gli altri: scrivi un programma che legge
+	un numero intero indicante l'età dello spettatore e visualizza l'importo che deve pagare.
+*/
+
+// Intestazione
 #include <iostream>
 using namespace std;
 
+// Dichiarazione
 int E;
 
+// Main
 int main(){
+	// Input
 	cout << "Eta: ";
 	cin >> E;
 
+	// Elaborazione e Output
 	cout << "Costo: ";
 	if(E<=10||E>65){
 		cout << 0;
 	}
-	else{
+	 else{
 		if(E<=18){
 			cout << 5;
 		}
-		else{
+		 else{
 			cout << 10;
-		};
-	};
+		 };
+	 };
 }
