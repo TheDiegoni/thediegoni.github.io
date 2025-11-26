72a8853bf603a3a198ee23bbfe28c5d515f5af2e
TheDiegoni <thediegoni575@gmail.com>
2025-11-26 09:41:18 +0100

    Refactor Patate.cpp for clarity and organization
    Refactor code to improve readability and structure.

--- Scripts/Patate.cpp
+++ Scripts/Patate.cpp
@@ -1,33 +1,48 @@
+/*
+	Autore: Diego Rocco
+	Descrizione: Leggi un numero che corrisponde al peso in kg di patate acquistate al mercato:
+		sapendo che ogni sacchetto può contenere al massimo 5 kg di patate e una cassetta fino a 30 kg,
+		determina il numero di sacchetti (massimo 4) o di cassette (massimo 2) necessari per poter portare
+		a casa la spesa.
+*/
+
+// Intestazione
 #include <iostream>
 using namespace std;
 
+// Dichiarazione
 float P;
 int S=0, C=0;
 const int SM=4, CM=2;
 
+
+// Main
 int main(){
+	// Input
 	cout << "Peso Patate: ";
 	cin >> P;
 
+	// Controllo
 	while(P>0){
 		if(C<2){
 			C+=1;
 			P-=30;
 		}
-		else{
+		 else{
 			if(S<4){
 				S+=1;
 				P-=5;
 			}
-			else{
+			 else{
 				cout << "Peso Eccessivo\n";
 				break;
-			};
-		};
+			 };
+		 };
 	};
 
+	// Output
 	cout << "Sacchetti: " << S;
 	cout << "\nCassette: " << C;
-
 	return 0;
 }
+
