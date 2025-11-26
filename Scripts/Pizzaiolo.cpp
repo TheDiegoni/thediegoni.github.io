1c5e5c4173f4a2f9850ce5032a1e6ab49485f56f
TheDiegoni <thediegoni575@gmail.com>
2025-11-26 09:58:04 +0100

    Refactor ingredient declarations and calculations
    Refactor ingredient quantities to use constants and remove redundant assignments.

--- Scripts/Pizzaiolo.cpp
+++ Scripts/Pizzaiolo.cpp
@@ -1,23 +1,35 @@
+/*
+	Autore: Diego Rocco
+	Descrizione:Con 4 hg di prosciutto, 1,4 Kg di pomodori, 1200 gr di farina 
+		si possono preparare: 4 Pizze Margherita e 8 Pizze al Prosciutto. 
+		Inserito in input  Numero delle pizze da preparare, calcolare 
+		il fabbisogno di ogni ingrediente , sapendo che tutte le 
+		pizze hanno lo stesso peso. 
+*/
+
+// Intestazione
 #include <iostream>
 using namespace std;
 
-int Farina, Pomodoro, Prosciutto, NumeroMargherite, NumeroProsciutto, TotalePizze;
+// Dichiarazione
+const int Farina=100, Pomodoro=350, Prosciutto=50;
+int NumeroMargherite, NumeroProsciutto, TotalePizze;
 
+// Main
 int main(){
-	Farina = 100;
-	Pomodoro = 350;
-	Prosciutto = 50;
-
+	// Input
 	cout << "Pizze Margherita: ";
 	cin >> NumeroMargherite;
 	cout << "\nPizze al Prosciutto: ";
 	cin >> NumeroProsciutto;
 
+	// Elaborazione
 	TotalePizze = NumeroMargherite+NumeroProsciutto;
 	Farina = Farina*TotalePizze;
 	Pomodoro = Pomodoro*NumeroMargherite;
 	Prosciutto = Prosciutto*NumeroProsciutto;
 
+	// Output
 	cout << "\nQuantità Farina: ";
 	cout << Farina;
 	cout << "\nQuantità Pomodoro: ";
@@ -25,6 +37,5 @@ int main(){
 	cout << "\nQuantità Prosciutto: ";
 	cout << Prosciutto;
 	cout << "\n";
-
 	return 0;
 }
