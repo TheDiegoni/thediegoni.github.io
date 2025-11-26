de97c30f2be56968e2b746a4a1f838a2b78393e8
TheDiegoni <thediegoni575@gmail.com>
2025-11-26 09:54:27 +0100

    Refactor if-else to switch statement for clarity

--- Scripts/Biancaneve.cpp
+++ Scripts/Biancaneve.cpp
@@ -1,46 +1,48 @@
+/*
+	Autore: Diego Rocco
+	Descrizione: Scrivi un programma che, inserendo un numero compreso tra 1 e 7, visualizza
+	il nome del nano corrispondente: per ogni altro numero visualizza "Biancaneve".
+*/
+
+// Intestazione
 #include <iostream>
 using namespace std;
 
+// Dichiarazione
 int N;
 
+
+// Main
 int main(){
-	cout << "Numero: ";
+	//Input
+	cout << "Inserisci un numero tra 1 e 7: ";
 	cin >> N;
 
-	if(N==1){
-		cout << "Dotto";
-	}
-	else{
-		if(N==2){
+	// Elaborazione e Output
+	switch(N){
+		case 1:
+			cout << "Dotto";
+			break;
+		case 2:
 			cout << "Brontolo";
-		}
-		else{
-			if(N==3){
-				cout << "Pisolo";
-			}
-			else{
-				if(N==4){
-					cout << "Mammolo";
-				}
-				else{
-					if(N==5){
-						cout << "Gongolo";
-					}
-					else{
-						if(N==6){
-							cout << "Eolo";
-						}
-						else{
-							if(N==7){
-								cout << "Cucciolo";
-							}
-							else{
-								cout << "Biancaneve";
-							};
-						};
-					};
-				};
-			};
-		};
+			break;
+		case 3:
+			cout << "Pisolo";
+			break;
+		case 4:
+			cout << "Mammolo";
+			break;
+		case 5:
+			cout << "Gongolo";
+			break;
+		case 6:
+			cout << "Eolo";
+			break;
+		case 7:
+			cout << "Cucciolo";
+			break;
+		default:
+			cout << "Biancaneve";
+			break;
 	};
 }
