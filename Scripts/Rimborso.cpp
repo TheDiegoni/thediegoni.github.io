f8d6e21a65f59f2785e17785d1d1df0749d7b6b4
TheDiegoni <thediegoni575@gmail.com>
2025-11-29 14:07:53 +0100

    Refactor input prompts and transport method check

--- Scripts/Rimborso.cpp
+++ Scripts/Rimborso.cpp
@@ -4,38 +4,46 @@ Autore: Diego Rocco
 Data: 13/11/2025
 Descrizione: Calcolo del rimborso scolastico sulle spese si spostamento.
 */
+
+// Intestazione
 #include <iostream>
 #include <string>
 using namespace std;
-// Main
+
+// Dichiarazione
 string N, P, M, PS;
 float C, D, R=0;
 const float RA=0.25, RT=0.3;
 
+// Main
 int main(){
-	cout << "Nome: ";
+	// Input Informazioni
+	cout << "Nome Studente: ";
 	cin >> N;
-	cout << "Costo: ";
+	cout << "Costo Abbonamento: ";
 	cin >> C;
 	cout << "Provincia Scuola: ";
 	cin >> PS;
 	cout << "Provincia Studente: ";
 	cin >> P;
 
+	// Controllo Provenienza
 	if(P==PS){
+		// Input e Controllo Distanza
 		cout << "Distanza dalla Scuola: ";
 		cin >> D;
 		if(D>=20){
-			cout << "Mezzo: ";
+			// Input e Controllo Mezzo di Trasporto
+			cout << "Mezzo (Bus, Treno o Altro): ";
 			cin >> M;
-			if(M=="Autobus"){
+			if(M=="Bus"){
 				R = C*RA;
 			}
-			else{
+			 else{
 				if(M=="Treno"){
 					R = C*RT;
 				};
-			};
+			 };
 		};
 	};
 
