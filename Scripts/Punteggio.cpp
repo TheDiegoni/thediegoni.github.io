a2f53e4aca1eb977bce9bf3d3338b31fb28e1127
TheDiegoni <thediegoni575@gmail.com>
2025-11-29 18:45:07 +0100

    Refactor score evaluation logic in Punteggio.cpp

--- Scripts/Punteggio.cpp
+++ Scripts/Punteggio.cpp
@@ -1,21 +1,33 @@
+/*
+	Autore: Diego Rocco
+	Descrizione: Viene fornito in input il punteggio di un candidato ad un concorso; se
+		è compreso tra 0 e 27, visualizzare RESPINTO, tra 28 e 40 visualizzare AMMESSO,
+		in tutti gli altri casi ERRORE.
+*/
+
+// Intestazione
 #include <iostream>
 using namespace std;
 
+// Dichiarazione
 int P;
 
+// Main
 int main(){
-	cout << "Punteggio: ";
+	// Input Punteggio
+	cout << "Inserisci il Punteggio: ";
 	cin >> P;
 
+	// Controllo e Output
 	if(P>=0&&P<=27){
 		cout << "Respinto";
 	}
-	else{
+	 else{
 		if(P>=28&&P<=40){
 			cout << "Ammesso";
 		}
-		else{
+		 else{
 			cout << "Errore";
-		};
-	};
+		 };
+	 };
 }
