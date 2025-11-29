22ec321522f27e5202ffd5a1326312ec10b57708
TheDiegoni <thediegoni575@gmail.com>
2025-11-29 22:13:32 +0100

    Enhance calculator input and initialize result variable
    Initialized variable S to 0 and added input prompts for numbers and operation selection.

--- Scripts/Calc.cpp
+++ Scripts/Calc.cpp
@@ -1,7 +1,6 @@
 /*
    Autore: Diego Rocco
-   Descrizione: Dati in input 2 numeri e
-      1 operazione, stampare la soluzione.
+   Descrizione: Dati in input 2 numeri e 1 operazione, stampare la soluzione.
 */
 
 // Intestazione
@@ -10,12 +9,34 @@ using namespace std;
 
 // Dichiarazione
 int OP;
-float N1, N2, S;
+float N1, N2, S=0;
 
 // Main
 int main(){
-   // Input
+   // Input Dati
    cout << "Inserisci il Primo Numero";
+   cin >> N1;
+   cout << "Inserisci il Secondo Numero";
+   cin >> N2;
+
+   // Selezione e Calcolo
+   cout << "Inserisci l'Operazione da Svolgere:\n1) Addizione\n2) Sottrazione\n3) Moltiplicazione\n4) Divisione\n";
+   cin >> OP;
+   switch(OP){
+      case 1:
+         S = N1+N2;
+         break;
+      case 2:
+         S = N1-N2;
+         break;
+      case 3:
+         S = N1*N2;
+         break;
+      case 4:
+         S = N1/N2;
+         break;
+   };
 
    // Output
+   cout << "Risultato: " << S;
 }
