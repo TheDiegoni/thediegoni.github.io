22f4bfde9c45938f7bd8dfb2d9b1cbf9cdcc3831
TheDiegoni <thediegoni575@gmail.com>
2025-11-29 15:27:55 +0100

    Correct output message for failing grade
    Fixed output message for failing student.

--- Scripts/Media.cpp
+++ Scripts/Media.cpp
@@ -1,20 +1,30 @@
+/*
+	Autore: Diego Rocco
+	Descrizione: Dati 2 voti, calcolare la media; se è sufficiente, comunicare
+		"Alunno Promosso", altrimenti comunicare "Alunno Rimandato"
+*/
+
+// Intestazione
 #include <iostream>
 using namespace std;
 
+// Dichiarazione
 float V1, V2, M;
 
+// Main
 int main(){
+	// Input Voti
 	cout << "Voto 1: ";
 	cin >> V1;
 	cout << "Voto 2: ";
 	cin >> V2;
 
+	// Calcolo e Confronto Media + Output
 	M = (V1+V2)/2;
-
 	if(M>=6){
 		cout << "Alunno Promosso";
 	}
-	else{
+	 else{
 		cout << "Alunno Bocciato";
-	};
+	 };
 }
