231b8eacb2e24eae3a760a1775bce679ab2f428f
TheDiegoni <thediegoni575@gmail.com>
2025-11-29 18:34:08 +0100

    Refine prompts and output for triangle validation
    Updated user prompts and output messages for clarity.

--- Scripts/Pitagora.cpp
+++ Scripts/Pitagora.cpp
@@ -1,21 +1,32 @@
+/*
+	Autore: Diego Rocco
+	Descrizione: Dati 3 numeri, stabilire se possono essere le misure dei lati di un
+		Triangolo Rettangolo.
+*/
+
+// Intestazione
 #include <iostream>
 #include <math.h>
 using namespace std;
 
+// Dichiarazione
 float NM, N1, N2;
 
+// Main
 int main(){
-	cout << "Inserisci Numero Maggiore: ";
+	// Input Dati
+	cout << "Inserisci il Numero Maggiore: ";
 	cin >> NM;
-	cout << "Inserisci Altro Numero: ";
+	cout << "Inserisci un Altro Numero: ";
 	cin >> N1;
-	cout << "Inserisci Altro Numero: ";
+	cout << "Inserisci un Altro Numero: ";
 	cin >> N2;
 
+	// Calcolo, Controllo e Output
 	if(NM==sqrt(N1*N1+N2*N2)){
-		cout << "Triangolo Rettangolo";
+		cout << "Può essere un Triangolo Rettangolo";
 	}
-	else{
-		cout << "Niente Rettangolo";
-	};
+	 else{
+		cout << "Non può essere un Triangolo Rettangolo";
+	 };
 }
