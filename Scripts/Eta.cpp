635d59e4cf6af1dd8a579b6be7d05c08f267bd42
TheDiegoni <thediegoni575@gmail.com>
2025-11-29 15:16:52 +0100

    Refactor Eta.cpp for clarity and structure
    Updated the program to improve clarity and structure, including detailed comments for better understanding.

--- Scripts/Eta.cpp
+++ Scripts/Eta.cpp
@@ -1,50 +1,65 @@
+/*
+	Autore: Diego Rocco
+	Descrizione: Dati in input i nomi, gli anni di nascita e le classi di 2 studenti,
+		calcolare la loro età media dopo aver confrontato le loro età stampando nome e
+		classe del più giovane.
+*/
+
+// Intestazione
 #include <iostream>
 #include <string>
 using namespace std;
 
+// Dichiarazione
 string nome1, classe1, nome2, classe2;
 int eta1, eta2, anno1, anno2, annoA;
 float etaM;
 
+// Main
 int main(){
+	// Permetti caratteri accentati in output
 	setlocale(LC_ALL, "italian");
 
-	cout << "Primo Nome: ";
+	// Input Informazioni 1° Studente
+	cout << "Nome Studente 1: ";
 	cin >> nome1;
-	cout << "Primo Anno: ";
+	cout << "Anno Studente 1: ";
 	cin >> anno1;
-	cout << "Prima Classe: ";
+	cout << "Classe Studente 1: ";
 	cin >> classe1;
-	cout << "Secondo Nome: ";
+
+	// Input Informazioni 2° Studente
+	cout << "Nome Studente 2: ";
 	cin >> nome2;
-	cout << "Secondo Anno: ";
+	cout << "Anno Studente 2: ";
 	cin >> anno2;
-	cout << "Seconda Classe: ";
+	cout << "Classe Studente 2: ";
 	cin >> classe2;
+
+	// Input Anno Attuale
 	cout << "Anno Attuale: ";
 	cin >> annoA;
 
+	// Calcolo Età
 	eta1 = annoA-anno1;
 	eta2 = annoA-anno2;
 	etaM = (eta1+eta2)/2;
-	cout << "Et� Media: ";
+	cout << "Età Media: ";
 	cout << etaM;
 
+	// Confronto e Output
 	if (eta1>eta2){
-		cout << "\nPi� Giovane: ";
-		cout << nome2;
-		cout << ", ";
-		cout << classe2;
+		cout << "\nStudente più Giovane: ";
+		cout << nome2 << ", " << classe2;
 	}
-	else{
+	 else{
 		if (eta1==eta2){
-			cout << "\nHanno la Stessa Et�";
+			cout << "\nI 2 Studenti hanno la Stessa Età";
 		}
-		else{
-			cout << "\nPi� Giovane: ";
+		 else{
+			cout << "\nStudente più Giovane: ";
 			cout << nome1 << ", " << classe1;
-		};
-	};
-
+		 };
+	 };
 	return 0;
 }
