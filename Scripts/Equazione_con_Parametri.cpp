fb6087648d9127cde53d5299c53c5a86b6eddb8f
TheDiegoni <thediegoni575@gmail.com>
2026-02-18 09:56:45 +0100

    Add files via upload

---
+++ Scripts/Equazione_con_Parametri.cpp
@@ -0,0 +1,31 @@
+/*
+    Autore: Diego Rocco
+    Traccia: Creare un Programma che applichi l'uso delle funzioni al
+        problema descritto implementando per ogni caso un sottoprogramma
+        specifico, considerando i casi:
+            1) Coefficiente di Secondo Grado = 0
+            2) Radici Reali e Coincidenti
+            3) Radici Reali e Distinte
+            4) Radici Immaginarie
+*/
+
+// Intestazione
+#include <iostream>
+#include <math>
+using namespace std;
+
+// Main
+int main(){
+    // Dichiarazione Locale
+    float A, B, C, Delta;
+
+    cout<<"Benvenuto! Questo programma ti richiederà i 3 valori noti necessari a risolvere un equazione secondo grado (inserisci 0 a tutti e 3 per terminare)\n\n"; // Titolo
+
+    // Ciclo Ripetizione Programma
+    do{
+        // Input Dati
+        cout<<"Inserisci il valore moltiplicato per"
+    }while(A!=0 || B!=0 || C!=0); // Fine Ciclo Ripetizione Programma
+
+    return 0;
+}
